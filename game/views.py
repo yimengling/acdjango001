85453b4e1694095e124b90c19115edf1cbb29bfe
翔 甘 <1964730890@qq.com>
2022-04-17 09:17:25 +0800

    my first

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style = "text-align: center">    1111！</h1>'
+    return HttpResponse(line1)
